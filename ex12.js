7ef71bfe6944152ffa0ff14b91e59b74967c8b4d
gitIsaacVinicius <v.isaacaraujo@gmail.com>
2025-09-20 19:10:29 -0300

    Resolução ex12

---
+++ ex12.js
@@ -0,0 +1,8 @@
+// 12. Percorrendo Arrays com for...of:
+// Crie um array de frutas e use for...of para mostrar cada fruta no console.
+
+let frutas = ['melancia', 'banana', 'manga']
+
+for (let fruta of frutas) {
+    console.log(fruta)
+}
