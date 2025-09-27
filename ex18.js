c578f455c3a650944b36af425ce75679ea29b58c
gitIsaacVinicius <v.isaacaraujo@gmail.com>
2025-09-27 13:10:07 -0300

    Resolução ex18

---
+++ ex18.js
@@ -0,0 +1,9 @@
+// 18. Ordenação (sort):
+// Dado o array [5, 1, 9, 3, 7], ordene-o em ordem crescente usando sort.
+
+
+const array = [5, 1, 9, 3, 7]
+
+array.sort()
+
+console.log(`Array ordernado: [${array}]`)
