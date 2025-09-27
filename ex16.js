d359d9c55836de94fa61e70d880bbd80badacea6
gitIsaacVinicius <v.isaacaraujo@gmail.com>
2025-09-27 13:08:59 -0300

    Resolução ex16

---
+++ ex16.js
@@ -0,0 +1,9 @@
+// 16. Dobrar Valores (map):
+// Dado o array [1, 2, 3, 4, 5], use map para criar um novo array com cada número
+// dobrado.
+
+const valores = [1, 2, 3, 4, 5]
+
+const valoresDobrados = valores.map(valor => valor * 2)
+
+console.log(`Os valores dobrados são: [${valoresDobrados}]`)
